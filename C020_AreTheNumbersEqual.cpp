2dfc0a33fb246af326754bcde3a3765519761160
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-28 13:06:29 -0400

    Are the Numbers Equal Completed Code
    Completed code with pseudocode.

---
+++ C020_AreTheNumbersEqual.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// Create a function that takes two integers and checks if they are equal.
+bool isEqual (int num1, int num2) {
+	while (num1 != num2) {
+		return false;
+	}
+	return true;
+}
