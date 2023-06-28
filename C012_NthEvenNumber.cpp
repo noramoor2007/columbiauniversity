087e417a642e9640aa94f4269ed50c863bb3520c
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-27 23:32:00 -0400

    Nth Even Number Completed Code
    Completed code with pseudocode.

---
+++ C012_NthEvenNumber.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+// Create a function that takes a number n and returns the nth even number beginning with 0 as the first.
+int nthEven (int n) {
+	return (n - 1) * 2; // It is always the number minus 1 multiplied by 2 because 0 counts as an even number.
+}
