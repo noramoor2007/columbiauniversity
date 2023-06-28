5e8c164d31fb4e11aaf10855b0a627e261eb565d
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-27 23:26:57 -0400

    Return the Last Element in an Array Completed Code
    Completed code with a bit of pseudocode.

---
+++ C011_ReturnTheLastElementInAnArray.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+// Create a function that accepts an array of numbers and returns the last number in the array.
+int getLastItem (int arr [], int length) {
+	return arr [length - 1];
+}
