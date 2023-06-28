3637528fdc9005d0c553486845deab05f3df65c8
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-27 23:52:03 -0400

    Char to ASCII Completed Code
    Completed code with pseudocode.

---
+++ C014_CharToASCII.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+// Create a function that returns the ASCII value of the passed in character.
+unsigned short ctoa (char c) {
+	return int (c); // You can find the ASCII value simplying by putting the input variable in the parenthesis of the data type "int".
+}
