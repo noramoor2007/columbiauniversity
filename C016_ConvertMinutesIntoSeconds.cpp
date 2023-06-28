f19bccff7586343e8d422b29b6c8c8853c8cb247
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-27 23:57:32 -0400

    Convert Minutes into Seconds Completed Code
    Completed code with pseudocode.

---
+++ C016_ConvertMinutesIntoSeconds.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+// Write a function that takes an integer minutes and converts it to seconds.
+function convert (minutes) {
+	return minutes * 60; // There are 60 seconds in one minute.
+}
