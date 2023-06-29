9305b7b5afccddc8e9de039cce2039d780aaa63c
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-29 12:02:40 -0400

    Updated Version of Electricity Bill Code

--- C031_ElectricityBill.cpp
+++ C031_ElectricityBill.cpp
@@ -4,8 +4,8 @@
 // Create a function that takes number of units consumed by the customer and returns calculated Electricity Bill as per following conditions; first 100 Units will be charged at $1/unit, next 100 Units will be charged at $2/unit, next 100 Units will be charged at $3/unit, next 200 Units will be charged at $4/unit, next Units will be charged at $5/unit, 10% tax to be added in final amount, and an extra $15 to be added for Meter Charge.
 double electricityBill (double units) {
 	double totalofbill = 0.0; // I created a variable called "totalofbill" that represents the total electricity bill that will be used to store the total. Right now it is just set equal to 0, and it is a double so it is set equal to 0.0.
-	double tax = 0.10; // This is the 10% tax.
 	double additionalcharge = 15.0;
+	double tax = 0.10;
 	if (units <= 100) { // If units is less than or equal to 100, then multiply units by 1 and set the total bill equal to that.
 		totalofbill = units * 1.0;
 	}
@@ -21,7 +21,6 @@ double electricityBill (double units) {
 	else {
 		totalofbill = 100.0 + 100.0 * 2.0 + 100.0 * 3.0 + 200.0 * 4.0 + (units - 500) * 5.0;
 	}
-    double taxamount = totalofbill * tax;
-    double result = totalofbill + taxamount + additionalcharge;
+    double result = totalofbill + totalofbill * tax + additionalcharge;
     return std :: ceil (result * 100) / 100.0; // This line returns the final calculated electricity bill. It uses "std :: ceil" to round up the "finalAmount" to two decimal places by multiplying by 100, taking the ceiling value, and then dividing by 100.0. The result is rounded up to the nearest cent. Round up to two decimal places.
 }
