a3a3ccc5bd3a374c6e79a96a6474508d72e7b626
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-29 15:19:28 -0400

    Updated Oddish vs. Evenish Code

--- C034_OddishVsEvenish.cpp
+++ C034_OddishVsEvenish.cpp
@@ -3,9 +3,9 @@
 std :: string oddishOrEvenish (int num) {
 	int sum = 0; // An integer variable called "sum" is declared and set equal to 0.
   while (num != 0) { // This while loop runs as long as "num" is not equal to 0.
-		sum += (num % 10); // While this is true, the "sum" is set equal to the "sum" plus the number mod 10.
-		num /= 10; // And then the number is set equal to the number divided by 10.
-	}
+	  sum += (num % 10); // While this is true, the "sum" is set equal to the "sum" plus the number mod 10.
+	  num /= 10; // And then the number is set equal to the number divided by 10.
+  }
 	if (sum % 2 == 0) { // This if-statement checks if the "sum" mod 2 is equal to 0, which is just checking if it is even or not.
 		return "Evenish"; // So if it is even, then the function, "oddishOrEvenish" will return "Evenish".
 	}
