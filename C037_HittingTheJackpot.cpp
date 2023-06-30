012752d7bf9c77bdeb3cff23381d67de5c481bb9
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-30 15:14:38 -0400

    Updated Version of Hitting the Jackpot

--- C037_HittingTheJackpot.cpp
+++ C037_HittingTheJackpot.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 // Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
 bool testJackpot (std :: vector <std :: string> result) {
-	for (int i = 0; i < result.size (); i--) {
-		if (result [i] != result [i + 1]) {
+	for (int i = 1; i < result.size (); i++) { // This is a for loop that creates the counter variable called "i" equal to 0. It is less than the result size.
+		if (result [i] != result [0]) { // This if-statement checks if the result value with an index of "i" is not equal to the value with an index of 0.
 			return false;
 		}
 	}
